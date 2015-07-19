57eb0ee44a1d323acf53607e3abddf3d0b4adf00
heshenghuan <heshenghuan@sina.com>
2015-07-19 13:10:50 +0800

    add record function

--- MyBody.py
+++ MyBody.py
@@ -6,6 +6,9 @@ Created on Sat Jul 18 19:03:00 2015
 """
 
 import math
+import getopt
+import sys
+import datetime
 
 class MyBody:
     def __init__(self):
@@ -16,13 +19,22 @@ class MyBody:
         self.bmi = []
         self.date = []
         
+        self.read_user_info(r"../userinfo/user.txt")
+        print "MyBody ver", self.VERSION,
+        print "personal weight supervisory program.",
+        print "@author: heshenghuan 2015"
+        print "-"*79
+        print "User's information:"
+        print "User:", self.user
+        print "Height(m):", self.height
+        
     def read_data(self, path):
         file_handler = open(path, 'r')
         for line in file_handler.readlines():
             data = line.split()            
             self.date.append(data[0])
-            self.weight.append(data[1])
-            self.bmi.append(data[2])
+            self.weight.append(float(data[1]))
+            self.bmi.append(float(data[2]))
         file_handler.close()
         
     def read_user_info(self, path):
@@ -30,4 +42,36 @@ class MyBody:
         line = file_handler.readline()
         self.user = line.split()[-1]
         line = file_handler.readline()
-        self.height = line.split()[-1]
+        self.height = float(line.split()[-1])
+        
+    def get_arg(self, arg):
+        try:
+            options,args = getopt.getopt(arg,"ha:",["help"])
+        except getopt.GetoptError:
+            print "argument error"
+        
+        for name, value in options:
+            if name in ('-h', '--help'):
+                print "help info"
+            if name in ('-a'):
+                self.add_record(value)
+    
+    def add_record(self, weight):
+        now = datetime.datetime.now()
+        date = now.strftime("%Y-%m-%d")
+        output = open(r"../userinfo/weights.txt","a")
+        output.write("\n")
+        output.write(date)
+        output.write(' ')
+        weight = "%.2f"%float(weight)
+        bmi = "%.4f"%(float(weight)/(self.height*self.height))
+        output.write(weight)
+        output.write(' ')
+        output.write(str(bmi))
+        output.write(' ')
+        output.close()
+        print "Add a record:",date,weight,bmi
+        
+if __name__ == "__main__":
+    case = MyBody()
+    case.get_arg(sys.argv[1:])
