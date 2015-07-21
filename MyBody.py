9ae1e175dc54b1413b758fdaa353fb2ad4521203
heshenghuan <heshenghuan@sina.com>
2015-07-21 23:43:01 +0800

    mybody now can draw the line chart of weight&bmi change

--- MyBody.py
+++ MyBody.py
@@ -5,7 +5,8 @@ Created on Sat Jul 18 19:03:00 2015
 @author: heshenghuan
 """
 
-import math
+import numpy as np
+import matplotlib.pyplot as pl
 import getopt
 import sys
 import datetime
@@ -13,7 +14,7 @@ import datetime
 class MyBody:
     def __init__(self):
         self.VERSION = 0.0
-        self.VERSION_DATE = "2015-07-19"
+        self.VERSION_DATE = "2015-07-21"
         self.user = ''
         self.height = 0.0
         self.weight = []
@@ -30,7 +31,7 @@ class MyBody:
         file_handler = open(path, 'r')
         for line in file_handler.readlines():
             data = line.split()            
-            self.date.append(data[0])
+            self.date.append(data[0][5:])
             self.weight.append(float(data[1]))
             self.bmi.append(float(data[2]))
         file_handler.close()
@@ -46,14 +47,15 @@ class MyBody:
         print "MyBody personal weight supervisory program.",
         print "ver",self.VERSION,self.VERSION_DATE
         print "@author: heshenghuan"
-        print "usage: python MyBody.py [options]"
-        print "options: -h               ->help"
-        print "         -a float         ->add weight record"
+        print "usage: python MyBody.py options"
+        print "options: -h           ->help"
+        print "         -a float     ->add weight record"
+        print "         -s           ->show the picture of weight change"
         
     def get_arg(self, arg):
         error = False
         try:
-            options,args = getopt.getopt(arg,"ha:")
+            options,args = getopt.getopt(arg,"ha:s")
         except getopt.GetoptError:
             error = True
         
@@ -64,6 +66,8 @@ class MyBody:
                 if name in ('-a'):
                     bmi = self.add_record(value)
                     self.bmi_analysis(bmi)
+                if name in ('-s'):
+                    self.show()
         else:
             #argument error, display help information
             print "Argument Error!"
@@ -103,6 +107,30 @@ class MyBody:
         if bmi>32:
             print "Obese"
             print "You are very fat. You need doctor."
+            
+    def show(self):
+        self.read_data(r"../userinfo/weights.txt")
+        x = range(len(self.date))
+        ax1 = pl.subplot(2,1,1)
+        ax2 = pl.subplot(2,1,2)
+        pl.sca(ax1)
+        pl.plot(x,self.weight,'r*')
+        pl.plot(x,self.weight,'b')
+        pl.xticks(x,self.date)
+        pl.xlabel("date")
+        pl.ylabel("weight/kg")
+        pl.ylim(50,80)
+        pl.title("weight change line chart",loc="right")
+        pl.sca(ax2)
+        pl.plot(x,self.bmi,'r*')
+        pl.plot(x,self.bmi,'b')
+        pl.xticks(x,self.date)
+        pl.xlabel("date")
+        pl.ylabel("BMI")
+        pl.ylim(15,40)
+        pl.title("BMI change line chart",loc="right")
+        pl.legend()
+        pl.show()
         
 if __name__ == "__main__":
     case = MyBody()
